c963d220372f78c453a94afde6b10c7aa97f1529
Aleksa <aleksa.ljujic2@gmail.com>
2024-08-17 18:14:03 +0200

    UI_Update
    Ui update Login, Register, User field and more...

--- Assets/Scenes/scripts/SceneScript.cs
+++ Assets/Scenes/scripts/SceneScript.cs
@@ -8,12 +8,30 @@ public class SceneScript : MonoBehaviour
     public GameObject Canvas;
     public GameObject CanvasRestart;
     public GameObject CanvasStart;
+    public GameObject CanvasLogin;
+    public GameObject CanvasRegister;
     public TMP_Text Win;
 
     public PuckScript puckScript;
     public PlayerMovement playerMovement;
     public ScoreScript scoreScript;
-    
+    public TMP_InputField inputFieldUsername;
+    public TMP_InputField inputFieldPassword;
+    public TMP_Text userText;
+    public TMP_Text WinsText;
+
+    private int p1Wins = 0;
+    public int P1Wins
+    {
+        get { return p1Wins; }
+        set { p1Wins = value; }
+    }
+
+    void Start()
+    {
+        inputFieldPassword.contentType = TMP_InputField.ContentType.Password;
+    }
+
     public void ShowRestartCanvas(bool p1Won)
     {
         Time.timeScale = 0;
@@ -22,7 +40,8 @@ public class SceneScript : MonoBehaviour
 
         if (p1Won)
         {
-            Win.text = "Player1 is winner!";
+            Win.text = userText.text + " is winner!";
+            WinsText.text = (++P1Wins).ToString();
         }
         else
         {
@@ -42,10 +61,53 @@ public class SceneScript : MonoBehaviour
 
     public void StartGame()
     {
-        Time.timeScale = 1;
+       if(inputFieldUsername.text != "" && inputFieldPassword.text != "")
+        {
+            Time.timeScale = 1;
+            userText.text = inputFieldUsername.text;
+            CanvasLogin.SetActive(false);
+            Canvas.SetActive(true);
+            CanvasRestart.SetActive(false);
+            CanvasStart.SetActive(false);
+        }
+        else
+        {
+            CanvasLogin.SetActive(true);
+        }
+        
+    }
 
-        Canvas.SetActive(true);
+    public void RegisterToLogin()
+    {
+        Time.timeScale = 0;
+
+        CanvasLogin.SetActive(true);
+        Canvas.SetActive(false);
         CanvasRestart.SetActive(false);
         CanvasStart.SetActive(false);
+        CanvasRegister.SetActive(false);
+    }
+    public void LoginToRegister()
+    {
+        Time.timeScale = 0;
+       
+        CanvasRegister.SetActive(true);
+        CanvasLogin.SetActive(false);
+        Canvas.SetActive(false);
+        CanvasRestart.SetActive(false);
+        CanvasStart.SetActive(false);
+    }
+    public void LogOut()
+    {
+        Time.timeScale = 0;
+
+        CanvasRegister.SetActive(false);
+        CanvasLogin.SetActive(true);
+        Canvas.SetActive(false);
+        CanvasRestart.SetActive(false);
+        CanvasStart.SetActive(false);
+        inputFieldUsername.text = "";
+        inputFieldPassword.text = "";
+        WinsText.text = "0";
     }
 }
